5702975c99e189c0d655890fdec3e09324d4cedf
Pastoray <pastoriaraar69@gmail.com>
2026-05-09 02:39:19 +0100

    fix: define bool, true, and false for compatibility

--- libp/defs.h
+++ libp/defs.h
@@ -6,10 +6,14 @@
 #define SYS_EXIT 60
 #define NULL ((void *)0)
 
+#define bool _Bool
+#define true 1
+#define false 0
+
 typedef signed short int16_t;
 typedef signed int int32_t;
 typedef signed long long int64_t;
-typedef signed short uint16_t;
+typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 typedef uint32_t size_t;
